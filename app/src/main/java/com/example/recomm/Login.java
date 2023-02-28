6521d37871ea6943c83d0264276a7b3f84ad53b3
seungchae1 <s2125@e-mirim.hs.kr>
2023-02-28 12:36:27 +0900

    로그인/회원가입 버튼 구현

--- app/src/main/java/com/example/recomm/Login.java
+++ app/src/main/java/com/example/recomm/Login.java
@@ -2,13 +2,37 @@ package com.example.recomm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
+
+    ImageButton imgbtn;
+    ImageButton imgbtn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        imgbtn = findViewById(R.id.imageButton);
+        imgbtn2 = findViewById(R.id.imageButton2);
+
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Login2.class);
+                startActivity(intent);
+            }
+        });
+        imgbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Join.class);
+                startActivity(intent);
+            }
+        });
     }
 }

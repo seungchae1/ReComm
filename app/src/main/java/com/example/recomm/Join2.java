0f4005e604bef0ddee4a7b639d93154756fa9628
seungchae1 <s2125@e-mirim.hs.kr>
2023-03-02 23:17:31 +0900

    회원가입 페이지1 비밀번호 입력창 수정

--- app/src/main/java/com/example/recomm/Join2.java
+++ app/src/main/java/com/example/recomm/Join2.java
@@ -10,18 +10,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-public class Join extends AppCompatActivity {
+public class Join2 extends AppCompatActivity {
 
     ImageButton imgbtn;
-    EditText edit;
-    boolean input;
+    EditText edit, edit2;
+    boolean input, input2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join);
+        setContentView(R.layout.activity_join2);
         imgbtn = findViewById(R.id.imageButton);
         edit = findViewById(R.id.edit);
-        input = false;
+        edit2 = findViewById(R.id.edit2);
+        input = input2 = false;
 
         edit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -38,22 +39,41 @@ public class Join extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 if(s.length() <= 0 ){
                     input = false;
-                    imgbtn.setImageResource(R.drawable.nredbtn);
                 }else{
                     input = true;
-                    imgbtn.setImageResource(R.drawable.redbtn);
                 }
             }
         });
+        edit2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() <= 0 ){
+                    input2 = false;
+                }else{
+                    input2 = true;
+                }
+            }
+        });
         imgbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(input){
-                    Intent intent = new Intent(getApplicationContext(), Join2.class);
+                if(input && input2){
+                    Intent intent = new Intent(getApplicationContext(), Join.class);
                     startActivity(intent);
                 }
             }
         });
+
     }
+
 }

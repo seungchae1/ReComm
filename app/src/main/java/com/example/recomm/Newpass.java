b0bf21c9b942499c1a46161cc3a100f22c971a82
seungchae1 <80873640+seungchae1@users.noreply.github.com>
2023-03-09 13:21:59 +0900

    비밀번호 재설정

--- app/src/main/java/com/example/recomm/Newpass.java
+++ app/src/main/java/com/example/recomm/Newpass.java
@@ -2,13 +2,95 @@ package com.example.recomm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Newpass extends AppCompatActivity {
 
+    ImageButton imgbtn;
+    EditText edit;
+    Spinner spin;
+    boolean input;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference db = mRootRef.child("user");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newpass);
+        imgbtn = findViewById(R.id.imageButton);
+        edit = findViewById(R.id.edit);
+        spin = (Spinner) findViewById(R.id.Spinn);
+        input = false;
+
+        String userM =edit.getText().toString();
+        String[] mail = getResources().getStringArray(R.array.spinner_array);
+        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner_array, android.R.layout.simple_spinner_item);
+        spin.setAdapter(arrayAdapter);
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        userM.concat("g");
+                        break;
+                    case 1:
+                        userM.concat("n");
+                        break;
+                    case 2:
+                        userM.concat("d");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() <= 0 ){
+                    input = false;
+                    imgbtn.setImageResource(R.drawable.nredbtn);
+                }else{
+                    input = true;
+                    imgbtn.setImageResource(R.drawable.redbtn);
+                }
+            }
+        });
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(input){
+                    Intent intent = new Intent(getApplicationContext(), Join2.class);
+                    intent.putExtra("userM",userM);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/recomm/Newpass2.java
@@ -0,0 +1,102 @@
+package com.example.recomm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Newpass2 extends AppCompatActivity {
+
+    ImageButton imgbtn;
+    ImageView imgv;
+    TextView textv;
+    EditText edit, edit2;
+    boolean samepass, input, input2;
+    TextInputLayout t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newpass2);
+        Intent getintent = getIntent();
+        String userM = getintent.getStringExtra("userM");
+        imgbtn = findViewById(R.id.imageButton);
+        edit = findViewById(R.id.edit);
+        edit2 = findViewById(R.id.edit2);
+        samepass = input = input2 = false;
+        t= findViewById(R.id.text2);
+        imgv = findViewById(R.id.imageView2);
+        textv = findViewById(R.id.textView5);
+
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() <= 0 ){
+                    input = false;
+                }else{
+                    input = true;
+                }
+                if(input && input2) imgbtn.setImageResource(R.drawable.redbtn);
+            }
+        });
+
+        edit2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() <= 0 ){
+                    input2 = false;
+                }else{
+                    input2 = true;
+                }
+                if(input && input2) imgbtn.setImageResource(R.drawable.redbtn);
+            }
+        });
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Join3.class);
+                String pass1 = edit.getText().toString();
+                String pass2 = edit2.getText().toString();
+                samepass = pass1.equals(pass2) && !pass1.equals("") && !pass2.equals("");
+                if(samepass){
+                    startActivity(intent);
+                }
+                else{
+                    t.setError(" ");
+                    t.setErrorIconDrawable(null);
+                    textv.setText("비밀번호를 확인해주세요.");
+                    imgv.setImageResource(R.drawable.error);
+                }
+            }
+        });
+    }
+
+}

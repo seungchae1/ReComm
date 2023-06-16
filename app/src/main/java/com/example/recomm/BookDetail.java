97d880f46d37d0cbd758f3a400142badef92673b
olsi10 <wangbampang2035@gmail.com>
2023-06-16 15:10:55 +0900

    검색 페이지 (토글만 하면 됨), 책 상세 페이지(아코디언만 하면됨) 오늘안에 끝냄ㄴㄴ다@!

--- app/src/main/java/com/example/recomm/BookDetail.java
+++ app/src/main/java/com/example/recomm/BookDetail.java
@@ -1,36 +1,46 @@
 package com.example.recomm;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 public class BookDetail extends AppCompatActivity {
 
+    private Button infoButton;
+    private LinearLayout accordionLayout;
+    private Button accordionButton;
+    private TextView bookInfoTextView;
 
+    private boolean isAccordionExpanded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_detail);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout accLayout = findViewById(R.id.acc);
-        TextView text1 = findViewById(R.id.text1);
-        LinearLayout panel1 = findViewById(R.id.panel1);
+        infoButton = findViewById(R.id.infoButton);
+        accordionLayout = findViewById(R.id.accordionLayout);
+        accordionButton = findViewById(R.id.accordionButton);
+        bookInfoTextView = findViewById(R.id.bookInfoTextView);
+    }
 
-        accLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (text1.getVisibility() == View.GONE) {
-                    text1.setVisibility(View.VISIBLE);
-                    panel1.setVisibility(View.VISIBLE);
-                } else {
-                    text1.setVisibility(View.GONE);
-                    panel1.setVisibility(View.GONE);
-                }
+    public void toggleAccordion(View view) {
+        if (view.getId() == R.id.infoButton) {
+            if (isAccordionExpanded) {
+                accordionLayout.setVisibility(View.GONE);
+                isAccordionExpanded = false;
+            } else {
+                accordionLayout.setVisibility(View.VISIBLE);
+                isAccordionExpanded = true;
             }
-        });
+        } else if (view.getId() == R.id.accordionButton) {
+            if (bookInfoTextView.getVisibility() == View.VISIBLE) {
+                bookInfoTextView.setVisibility(View.GONE);
+            } else {
+                bookInfoTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
